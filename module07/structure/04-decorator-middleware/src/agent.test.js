8fc618b65e091e0628e7e0dc36f8c20df354835a
kissinger156 <severo.e.carlos@gmail.com>
2022-11-21 09:27:31 -0300

    adjust: add some comments on agent test

--- module07/structure/04-decorator-middleware/src/agent.test.js
+++ module07/structure/04-decorator-middleware/src/agent.test.js
@@ -14,26 +14,34 @@ describe('Http interceptor agent', () => {
   beforeEach(() => jest.clearAllMocks())
 
   test('should not change header', () => {
+    //mocking our setHeader function
     const response = {
       setHeader: jest.fn().mockReturnThis()
     }
 
+    //instancing our mocked http module
     const serverInstance = new originalHttp.Server();
     serverInstance.emit(eventName, request, response);
 
+    //checking if the setHeader function hasn't be called
     expect(response.setHeader).not.toHaveBeenCalled()
   })
 
   test('should activate header interceptor', () => {
+    //running our interceptor to change the default emit behavior
     InjectHttpInterceptor()
 
+    //mocking our setHeader function
     const response = {
       setHeader: jest.fn().mockReturnThis()
     }
 
+    //instancing the default Server module
     const serverInstance = new Server();
+
     serverInstance.emit(eventName, request, response)
 
+    //checking if the setHeader has be called with the specific argument 
     expect(response.setHeader).toHaveBeenCalledWith('X-Instrumented-By', 'Csevero')
   })
 })
