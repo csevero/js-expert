938b26221fc53192e1f4a07770f2e9dffb277411
kissinger156 <severo.e.carlos@gmail.com>
2022-11-29 22:36:59 -0300

    feature: added class about npm package
    This class show how we can build, deploy and work with project versions using the npm

---
+++ module08/01-my-first-npm-module/myapp/index.js
@@ -0,0 +1,7 @@
+import FluentSQLBuilder from '@kissinger156/fluentsql'
+
+import database from './database/data.json'
+
+const result = FluentSQLBuilder.for(database).where({ registered: /^(2020|2019)/ }).select(['category']).limit(3).groupCount('category').build()
+
+console.log(result)
