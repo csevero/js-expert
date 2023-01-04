337ebee8b7e227de8087181ae294bdc4026c45d8
kissinger156 <severo.e.carlos@gmail.com>
2023-01-04 16:57:47 -0300

    feature: added class about frameworks to get user input
    In the class we talk about creating applications for terminal using the native nodejs or using external libs to do it

---
+++ module09/03-native-vs-yargs/yargs/cli-yargs.mjs
@@ -0,0 +1,36 @@
+#!/usr/bin/env node
+
+import yargs from 'yargs';
+import { hideBin } from 'yargs/helpers';
+
+const hero = ({ name, age, power }) => ({ name, age, power, id: Date.now() });
+const { argv } = yargs(hideBin(process.argv))
+  .command('createHero', 'create a hero', builder => {
+    return builder
+      .option('name', {
+        alias: 'n',
+        demandOption: true,
+        describe: 'hero name',
+        type: 'string',
+      })
+      .option('age', {
+        alias: 'a',
+        demandOption: true,
+        describe: 'hero age',
+        type: 'number',
+      })
+      .option('power', {
+        alias: 'p',
+        demandOption: true,
+        describe: 'hero power',
+        type: 'string',
+      })
+      .example(
+        'createHero --name Flash --age 24 --power Speed',
+        'create a hero',
+      )
+      .example('createHero --n Flash --a 24 --p Speed', 'create a hero');
+  })
+  .epilog('copyright 2023');
+
+console.log(hero(argv));
